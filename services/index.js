b48f675f535ef1d7bfa08506285db57f405a4695
YannisDoerig <you@example.com>
2022-10-09 13:52:09 +0200

    update home

---
+++ services/index.js
@@ -0,0 +1,17 @@
+import { addDoc, collection, Timestamp } from "firebase/firestore/lite";
+import { firestore } from "firebaseConfig";
+
+export const sendContactForm = async ({ name, email }) => {
+  try {
+    const ref = collection(firestore, "contact");
+    await addDoc(ref, {
+      name,
+      email,
+      sentAt: Timestamp.now().toDate(),
+    });
+    return 0;
+  } catch (err) {
+    console.log(err);
+    return -1;
+  }
+};
