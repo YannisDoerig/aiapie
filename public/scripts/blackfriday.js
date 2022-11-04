1cc1ff216625e6659b6408b66a06eb039405357d
YannisDoerig <you@example.com>
2022-11-04 13:55:37 +0100

    update google tag

--- public/scripts/blackfriday.js
+++ public/scripts/blackfriday.js
@@ -23,35 +23,89 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+let allOffers = true;
+let country = "";
 
-function handleForm(event) {
-  console.log("handle form started...");
+function handleFirstForm(event) {
   event.preventDefault();
-  let newName = document.getElementById("newName").value;
-  let newEmail = document.getElementById("newEmail").value;
-  if (newName != undefined && newEmail != undefined) {
-    hideForm();
-    submitRegistration(newName, newEmail);
+  country = document.getElementById("countries").value;
+  if (country != undefined) {
+    goToStepTwo();
   } else {
     console.log("one of the values was undefined");
   }
 }
 
-const submitRegistration = async function (newName, newEmail) {
-  // Add a new document with a generated id.
-  const docRef = await addDoc(collection(firestore, "aiapie-blackfriday"), {
-    name: newName,
-    email: newEmail,
-  });
+function handleLastForm(event) {
+  event.preventDefault();
+  let newName = document.getElementById("newName").value;
+  let newEmail = document.getElementById("newEmail").value;
+  if (newName != undefined && newEmail != undefined) {
+    hideForm();
+    submitRegistration(country, newName, newEmail);
+  } else {
+    console.log("one of the values was undefined");
+  }
+}
+
+function goToStepTwo() {
+  let step1 = document
+    .getElementById("funnel-step-one")
+    .classList.add("hidden");
+  let step2 = document
+    .getElementById("funnel-step-two")
+    .classList.remove("hidden");
+}
+function updateOfferOption() {
+  allOffers = false;
+  goToStepThree();
+}
+function goToStepThree() {
+  let step2 = document
+    .getElementById("funnel-step-two")
+    .classList.add("hidden");
+  let step3 = document
+    .getElementById("funnel-step-three")
+    .classList.remove("hidden");
+}
+
+const submitRegistration = async function (country, newName, newEmail) {
+  if (allOffers) {
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(firestore, "aiapie-all-offers"), {
+      name: newName,
+      email: newEmail,
+      country: country,
+    });
+  } else {
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(firestore, "aiapie-blackfriday"), {
+      name: newName,
+      email: newEmail,
+      country: country,
+    });
+  }
+
   console.log("Document written with ID: ", docRef.id);
 };
 
-let form = document
+let step1 = document
+  .getElementById("country-form")
+  .addEventListener("submit", handleFirstForm);
+
+let step2btn1 = document
+  .getElementById("allOffersBtn")
+  .addEventListener("click", goToStepThree);
+let step2btn2 = document
+  .getElementById("bfOffersBtn")
+  .addEventListener("click", updateOfferOption);
+
+let step3 = document
   .getElementById("email-form")
-  .addEventListener("submit", handleForm);
+  .addEventListener("submit", handleLastForm);
 
 function hideForm() {
-  let form = (document.getElementById("email-form").style.display = "none");
+  let form3 = (document.getElementById("email-form").style.display = "none");
 
   document.getElementById("newsletter-title").innerHTML =
     "You Registered Successfully!";
